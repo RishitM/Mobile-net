a81f9b54a64b6328e74333e17b8b2ee6fa75ff0d
RishitM <72683716+RishitM@users.noreply.github.com>
2021-03-10 19:31:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var answer = "";
 console.log("ml5 version = ",ml5.version);
 Webcam.set(
     width=300,
@@ -20,8 +21,14 @@ function Modeloaded(){
 function identifyPic(){
 capturedImage=document.getElementById("pic");
 classifier.classify(capturedImage, gotResult);
-//classifier.classify(capturedImage,gotResult);
      
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data1="I think the item you are showing is "+ answer;
+    var utterThis= new SpeechSynthesisUtterance(speak_data1) ;
+    utterThis.rate=0.7;
+    synth.speak(utterThis);
 }
 function gotResult(error,result){
     if(error){
@@ -29,5 +36,9 @@ function gotResult(error,result){
     }
     else{
         console.log(result);
+        answer= result[0].label;
+        console.log(answer);
+        document.getElementById("answer").innerHTML= "<img src='blub.gif' id='pic2'>" + answer;
+        speak();
     }
 }
